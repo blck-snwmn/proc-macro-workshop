ea1b26288f3697398cc942a21bd2cff3a8807a95
blck-snwmn <whitesnowmancreator@gmail.com>
2021-01-31 22:54:24 +0900

    Done 02. impl builder that return empty by quote!

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    TokenStream::new()
+    let q = quote! {
+        impl Command {
+            pub fn builder() {}
+        }
+    };
+    q.into()
 }
