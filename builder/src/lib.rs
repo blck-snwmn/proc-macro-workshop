03a35b02185ae1dca0a3ad2efd29a2bef98d4123
blck-snwmn <whitesnowmancreator@gmail.com>
2021-02-08 00:24:27 +0900

    Done 06. Option field

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,9 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use std::error::Error;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
+use syn::{
+    parse_macro_input, Data, DeriveInput, Fields, FieldsNamed, GenericArgument, PathArguments, Type,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -51,10 +53,20 @@ fn quote_setter(data: &Data) -> Result<proc_macro2::TokenStream, Box<dyn Error>>
             let xx = fnamed.named.iter().map(|x| {
                 let name = &x.ident;
                 let ty = &x.ty;
-                quote! {
-                    pub fn #name(&mut self, #name: #ty) ->&mut Self{
-                        self.#name =  std::option::Option::Some(#name);
-                        self
+                if contain_option_type(&x.ty) {
+                    let inty = extract_option_generics_type(&x.ty).unwrap();
+                    quote! {
+                        pub fn #name(&mut self, #name: #inty) ->&mut Self{
+                            self.#name =  std::option::Option::Some(#name);
+                            self
+                        }
+                    }
+                } else {
+                    quote! {
+                        pub fn #name(&mut self, #name: #ty) ->&mut Self{
+                            self.#name =  std::option::Option::Some(#name);
+                            self
+                        }
                     }
                 }
             });
@@ -76,8 +88,16 @@ fn quote_builder_fields(data: &Data) -> Result<proc_macro2::TokenStream, Box<dyn
                 // quote! {
                 //     #x.ident: std::option::Option<#x.ty>
                 // }
-                quote! {
-                    #name: std::option::Option<#ty>
+                if contain_option_type(&x.ty) {
+                    // builderのフィールドはすべてOption型にする
+                    // 対象のフィールドがOptionの場合、builderのフィールドをOptionで包む必要がないので、そのまま指定
+                    quote! {
+                        #name: #ty
+                    }
+                } else {
+                    quote! {
+                        #name: std::option::Option<#ty>
+                    }
                 }
             });
             return quote! {
@@ -110,14 +130,48 @@ fn quote_build_fields(data: &Data) -> Result<proc_macro2::TokenStream, Box<dyn E
         return fields(&ds.fields).map(|fnamed| {
             let xx = fnamed.named.iter().map(|x| {
                 let name = &x.ident;
-                quote! {
-                    #name: self.#name.take().unwrap()
+                if contain_option_type(&x.ty) {
+                    quote! {
+
+                        #name: self.#name.take()
+                    }
+                } else {
+                    quote! {
+                        #name: self.#name.take().unwrap()
+                    }
                 }
             });
             return quote! {
                 #(#xx,)*
             };
         });
+    }
+    Err("a")?
+}
+
+fn contain_option_type(ty: &Type) -> bool {
+    if let Type::Path(p) = ty {
+        return p.path.segments.iter().any(|ps| ps.ident == "Option");
+    }
+    false
+}
+
+fn extract_option_generics_type(ty: &Type) -> Result<&Type, Box<dyn Error>> {
+    if let Type::Path(p) = ty {
+        let arg = p.path.segments.iter().find(|ps| ps.ident == "Option");
+        let ex = arg
+            .map(|x| {
+                if let PathArguments::AngleBracketed(garg) = &x.arguments {
+                    if let GenericArgument::Type(t) = garg.args.first().unwrap() {
+                        return Some(t);
+                    }
+                }
+                None
+            })
+            .flatten();
+        if let Some(t) = ex {
+            return Ok(t);
+        }
     }
     Err("a")?
 }
