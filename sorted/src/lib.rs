4759f3d0460bcab869d51e45afa4e648d8306e17
blck-snwmn <whitesnowmancreator@gmail.com>
2021-02-12 03:14:46 +0900

    Check Sorted

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, Error, Item};
+use syn::{parse_macro_input, spanned::Spanned, AttributeArgs, Error, Item, Variant};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -10,7 +11,33 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
 
     match &input {
-        Item::Enum(_) => {}
+        Item::Enum(item) => {
+            let sorted = item.variants.iter().fold(
+                Ok(None),
+                |acc_before: Result<Option<&Variant>, TokenStream2>, now| {
+                    acc_before.and_then(|x| match x {
+                        Some(before) => {
+                            let before_indent = before.ident.to_string();
+                            let now_indent = now.ident.to_string();
+                            if before_indent <= now_indent {
+                                Ok(Some(now))
+                            } else {
+                                Err(Error::new(
+                                    now.span(),
+                                    // TODO ここは変数から取得する
+                                    format!("SomethingFailed should sort before ThatFailed"),
+                                )
+                                .into_compile_error())
+                            }
+                        }
+                        None => Ok(Some(now)),
+                    })
+                },
+            );
+            if sorted.is_err() {
+                return sorted.err().unwrap().into();
+            }
+        }
         _ => {
             // TODO この部分は関数として切り出すこと
             return Error::new_spanned(quote! {"#[sorted]"}, "expected enum or match expression")
