977c3a8c03fdec871885e994f26393f504dbca7b
blck-snwmn <whitesnowmancreator@gmail.com>
2021-02-12 02:20:24 +0900

    Done 02-not-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,13 +1,22 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, ItemEnum};
+use syn::{parse_macro_input, AttributeArgs, Error, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as ItemEnum);
+    let input = parse_macro_input!(input as Item);
     let args = parse_macro_input!(args as AttributeArgs);
 
     let _ = args;
+
+    match &input {
+        Item::Enum(_) => {}
+        _ => {
+            return Error::new_spanned(quote! {"#[sorted]"}, "expected enum or match expression")
+                .to_compile_error()
+                .into()
+        }
+    }
 
     let q = quote! {
         #input
