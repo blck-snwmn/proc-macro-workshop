cf4cf60b3918a1754efd83b433e0ee9efa3fb728
blck-snwmn <whitesnowmancreator@gmail.com>
2021-02-12 01:41:58 +0900

    Use ItemEnum, AttributeArgs remote DeriveInput

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,10 +1,12 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, AttributeArgs, ItemEnum};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as ItemEnum);
+    let args = parse_macro_input!(args as AttributeArgs);
+
     let _ = args;
 
     let q = quote! {
