e867c7015e7bb02cdfb4f703cf7491c8643b0763
blck-snwmn <whitesnowmancreator@gmail.com>
2021-02-13 19:55:57 +0900

    Done 05-match-expr

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,7 +1,10 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, TokenStream as TokenStream2};
-use quote::quote;
-use syn::{parse_macro_input, spanned::Spanned, AttributeArgs, Error, Item, ItemEnum};
+use quote::{quote, ToTokens};
+use syn::{
+    parse_macro_input, spanned::Spanned, visit_mut::VisitMut, AttributeArgs, Error, ExprMatch,
+    Item, ItemEnum,
+};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -27,6 +30,7 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
 
     output
 }
+
 // ソートされているかチェック
 fn sorted_variants(item: &ItemEnum) -> Result<(), TokenStream2> {
     let origin: Vec<&Ident> = item.variants.iter().map(|v| &v.ident).collect();
@@ -46,4 +50,84 @@ fn sorted_variants(item: &ItemEnum) -> Result<(), TokenStream2> {
             .into_compile_error())
         }
     })
+}
+
+struct Visitor {
+    err: Option<TokenStream2>,
+}
+impl syn::visit_mut::VisitMut for Visitor {
+    fn visit_expr_match_mut(&mut self, em: &mut ExprMatch) {
+        if !em.attrs.iter().any(|x| x.path.is_ident("sorted")) {
+            return syn::visit_mut::visit_expr_match_mut(self, em);
+        }
+        // remove sorted attribute
+        em.attrs.retain(|a| !a.path.is_ident("sorted"));
+
+        em.arms.iter().for_each(|x| match &x.pat {
+            syn::Pat::TupleStruct(pts) => {
+                println!("{}", pts.path.to_token_stream().to_string())
+            }
+            _ => println!(),
+        });
+
+        let original: Vec<&syn::Path> = em
+            .arms
+            .iter()
+            .filter_map(|a| match &a.pat {
+                syn::Pat::TupleStruct(pts) => Some(&pts.path),
+                _ => {
+                    println!("no exptected");
+                    None
+                }
+            })
+            .collect();
+        let mut sorted = original.clone();
+        sorted.sort_by(|l, r| {
+            let l = l.to_token_stream().to_string();
+            let r = r.to_token_stream().to_string();
+            l.cmp(&r)
+        });
+
+        if let Err(ts) = original
+            .iter()
+            .zip(sorted.iter())
+            .try_fold((), |_, (o, s)| {
+                if o.to_token_stream().to_string() == s.to_token_stream().to_string() {
+                    Ok(())
+                } else {
+                    Err(Error::new(
+                        s.span(),
+                        // TODO ここは変数から取得する
+                        format!(
+                            "{} should sort before {}",
+                            s.to_token_stream().to_string(),
+                            o.to_token_stream().to_string()
+                        ),
+                    )
+                    .into_compile_error())
+                }
+            })
+        {
+            self.err = Some(ts);
+        }
+
+        syn::visit_mut::visit_expr_match_mut(self, em);
+    }
+}
+
+#[proc_macro_attribute]
+pub fn check(_: TokenStream, input: TokenStream) -> TokenStream {
+    // let output = input.clone();
+
+    let mut input = parse_macro_input!(input as syn::ItemFn);
+    let mut v = Visitor { err: None };
+    v.visit_item_fn_mut(&mut input);
+
+    let mut output: TokenStream = (quote! {#input}).into();
+
+    if let Some(e) = v.err {
+        output.extend(TokenStream::from(e));
+    }
+
+    output
 }
