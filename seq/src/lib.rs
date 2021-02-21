b25ffc352fc170d3e988056d581e18aa6b2d0f0b
blck-snwmn <whitesnowmancreator@gmail.com>
2021-02-22 01:16:29 +0900

    Done 01-parse-header.rs

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    unimplemented!()
+    let out = quote! {};
+    out.into()
 }
